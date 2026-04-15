66006f49e9e7d08e41fdba877d9204b113af6575
Hayaya <hanyang.hayaya@gmail.com>
2026-04-15 11:44:09 -0700

    Updated Trade Scene with SFX
    - Added sfx
    - Added in-game text instructions
    - Updated visuals

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource sell;
+
+    void Start()
+    {
+        Locator.Instance.Controller.UpdateCoin += HandleUpdateCoin;
+    }
+
+    void HandleUpdateCoin(int x)
+    {
+        sell.Play();
+    }
+}
