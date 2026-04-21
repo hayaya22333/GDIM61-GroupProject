95063b28079a247add2f6903c2046bb43767fd00
tinamengxq <tinam5@uci.edu>
2026-04-20 19:10:04 -0700

    prepare
    - detailed description of cards

--- Assets/Scripts/Controllers/PrepareController.cs
+++ Assets/Scripts/Controllers/PrepareController.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PrepareController : MonoBehaviour
 {
+    public TMP_Text detail;
+
+    public void Start()
+    {
+        detail.enabled = false;
+    }
     public void Click(int i)
     {
         SceneManager.LoadScene(i);
     }
 
-    
+    public void Click()
+    {
+        detail.enabled = true;
+    }
+
 }
--- Assets/Scripts/Prepare/ChoiceCard.cs
+++ Assets/Scripts/Prepare/ChoiceCard.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class ChoiceCard : DragObject
 {
-
+    public CardNode cardNode;
+    public string cardDesciption;
+    //写一段对应的card description
+    public PrepareController prepareController;
+    public void ShowTextDetail()
+    {
+        prepareController.detail.text = cardDesciption;
+        Debug.Log("click");
+    }
 }
