b8e57d74828563ecc68989acbda0a806e924b8b2
Hayaya <hanyang.hayaya@gmail.com>
2026-05-11 22:29:31 -0700

    Added audio for combat

--- Assets/Scripts/Combat/AudioManagerCombat.cs
+++ Assets/Scripts/Combat/AudioManagerCombat.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+public class AudioManagerCombat : MonoBehaviour
 {
     [SerializeField] private AudioSource heal;
     [SerializeField] private AudioSource hurt;
