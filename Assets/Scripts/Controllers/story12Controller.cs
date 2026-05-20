ab1b82aa165ce18dd86f67a00601587b7554e07e
tinamengxq <tinam5@uci.edu>
2026-05-19 17:37:23 -0700

    debug

--- Assets/Scripts/Controllers/story12Controller.cs
+++ Assets/Scripts/Controllers/story12Controller.cs
@@ -37,7 +37,7 @@ public class story12Controller : MonoBehaviour
         }
         else
         {
-            StoryEnd();
+            NextScene();
         }
     }
 
--- Assets/Scripts/Controllers/story23Controller.cs
+++ Assets/Scripts/Controllers/story23Controller.cs
@@ -37,7 +37,7 @@ public class story23Controller : MonoBehaviour
         }
         else
         {
-            StoryEnd();
+            NextScene();
         }
     }
 
