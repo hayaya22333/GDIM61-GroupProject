ef0f9e2f4558e2d5917022ed7a50a2398aa85fdb
tinamengxq <tinam5@uci.edu>
2026-05-19 17:02:01 -0700

    story all

--- Assets/Scripts/Controllers/story12Controller.cs
+++ Assets/Scripts/Controllers/story12Controller.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class story12Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private List<Sprite> story = new List<Sprite>();
+    [SerializeField] private GameObject continueButton;
+    [SerializeField] private GameObject clickArea;
+    [SerializeField] private Image image;
+
+    private int imageIndex = 0;
+    private bool storyFinish = false;
     void Start()
     {
         GameController.Instance.gameProcess = GameController.GameProcess.Level2;
+        continueButton.SetActive(false);
+        clickArea.SetActive(true);
+        imageIndex = 0;
+        storyFinish = false;
+        image.sprite = story[imageIndex];
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NextSprite()
     {
-        
+        if (storyFinish == true)
+        {
+            return;
+        }
+
+        imageIndex += 1;
+        if (imageIndex < story.Count)
+        {
+            image.sprite = story[imageIndex];
+        }
+        else
+        {
+            StoryEnd();
+        }
     }
+
+    public void StoryEnd()
+    {
+        storyFinish = true;
+        clickArea.SetActive(false);
+        continueButton.SetActive(true);
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
 }
